2dcc17fb9d3a7e4304324e5cf04a6c7d15ee5774
Santosh <santosh.chauhan@ymedialabs.com>
2018-01-22 17:47:02 +0530

    added comment

--- app/src/main/java/com/droidpro/barcodescanner/MainActivity.java
+++ app/src/main/java/com/droidpro/barcodescanner/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButtonScan=findViewById(R.id.btn_scan);
+        //Launching activity
         mButtonScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
